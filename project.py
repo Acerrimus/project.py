62e079d6790e4d9ec25bf2c99c02e2f782db8eca
Acerrimus <sahilsingh.contact@gmail.com>
2024-07-16 14:55:45 +0100

    With plot function (not interactive)

--- project.py
+++ project.py
@@ -2,9 +2,11 @@ import requests
 import json
 import sys
 from argparse import ArgumentParser
+import matplotlib.pyplot as plt
 
 
-country_list =  {
+
+country_currency =  {
      
     'usa': 'USD',  # United States Dollar
     'italy': 'EUR',  # Euro
@@ -59,13 +61,182 @@ country_list =  {
     'latvia': 'EUR',  # Euro
     'lithuania': 'EUR',  # Euro
 }
+country_codes = {
+    "Afghanistan": "af",
+    "Albania": "al",
+    "Algeria": "dz",
+    "American Samoa": "as",
+    "Anguilla": "ai",
+    "Australia": "au",
+    "Austria": "at",
+    "Bahamas": "bs",
+    "Bahrain": "bh",
+    "Bangladesh": "bd",
+    "Barbados": "bb",
+    "Belarus": "by",
+    "Belgium": "be",
+    "Belize": "bz",
+    "Benin": "bj",
+    "Bermuda": "bm",
+    "Bhutan": "bt",
+    "Bosnia And Herzegovina": "ba",
+    "Bouvet Island": "bv",
+    "Brazil": "br",
+    "British Indian Ocean Territory": "io",
+    "Brunei Darussalam": "bn",
+    "Bulgaria": "bg",
+    "Burkina Faso": "bf",
+    "Burundi": "bi",
+    "Cambodia": "kh",
+    "Cameroon": "cm",
+    "Canada": "ca",
+    "Cape Verde": "cv",
+    "Central African Republic": "cf",
+    "Chile": "cl",
+    "China": "cn",
+    "Colombia": "co",
+    "Comoros": "km",
+    "Congo, Democratic Republic Of The": "cd",
+    "Congo, Republic Of The": "cg",
+    "Costa Rica": "cr",
+    "Cote D'ivoire": "ci",
+    "Croatia": "hr",
+    "Cuba": "cu",
+    "Cyprus": "cy",
+    "Czech Republic": "cz",
+    "Denmark": "dk",
+    "Djibouti": "dj",
+    "Dominican Republic": "do",
+    "Ecuador": "ec",
+    "Egypt": "eg",
+    "El Salvador": "sv",
+    "Eritrea": "er",
+    "Ethiopia": "et",
+    "Europe": "eu",
+    "Fiji": "fj",
+    "Finland": "fi",
+    "France": "fr",
+    "French Guiana": "gf",
+    "French Polynesia": "pf",
+    "Gambia": "gm",
+    "Germany": "de",
+    "Ghana": "gh",
+    "Greece": "gr",
+    "Guatemala": "gt",
+    "Guinea": "gn",
+    "Guyana": "gy",
+    "Honduras": "hn",
+    "Hong Kong": "hk",
+    "Hungary": "hu",
+    "Iceland": "is",
+    "India": "in",
+    "Indonesia": "id",
+    "Iran": "ir",
+    "Iraq": "iq",
+    "Ireland": "ie",
+    "Israel": "il",
+    "Italy": "it",
+    "Japan": "jp",
+    "Jordan": "jo",
+    "Kazakhstan": "kz",
+    "Kenya": "ke",
+    "Kuwait": "kw",
+    "Laos": "la",
+    "Latvia": "lv",
+    "Lebanon": "lb",
+    "Lesotho": "ls",
+    "Liberia": "lr",
+    "Liechtenstein": "li",
+    "Lithuania": "lt",
+    "Luxembourg": "lu",
+    "Macedonia": "mk",
+    "Madagascar": "mg",
+    "Malawi": "mw",
+    "Malaysia": "my",
+    "Maldives": "mv",
+    "Mali": "ml",
+    "Malta": "mt",
+    "Mauritania": "mr",
+    "Mauritius": "mu",
+    "Mexico": "mx",
+    "Micronesia, Federated States Of": "fm",
+    "Moldova": "md",
+    "Monaco": "mc",
+    "Montenegro": "me",
+    "Montserrat": "ms",
+    "Morocco": "ma",
+    "Myanmar": "mm",
+    "Namibia": "na",
+    "Nepal": "np",
+    "Netherlands": "nl",
+    "New Zealand": "nz",
+    "Niger": "ne",
+    "Nigeria": "ng",
+    "North Korea": "kp",
+    "Norway": "no",
+    "Oman": "om",
+    "Pakistan": "pk",
+    "Palestinian Territory": "ps",
+    "Paraguay": "py",
+    "Peru": "pe",
+    "Philippines": "ph",
+    "Poland": "pl",
+    "Portugal": "pt",
+    "Qatar": "qa",
+    "Romania": "ro",
+    "Russia": "ru",
+    "Rwanda": "rw",
+    "Saint Kitts And Nevis": "kn",
+    "Saint Vincent And The Grenadines": "vc",
+    "San Marino": "sm",
+    "Saudi Arabia": "sa",
+    "Senegal": "sn",
+    "Serbia": "rs",
+    "Seychelles": "sc",
+    "Sierra Leone": "sl",
+    "Singapore": "sg",
+    "Slovakia": "sk",
+    "Slovenia": "si",
+    "Solomon Islands": "sb",
+    "Somalia": "so",
+    "South Africa": "za",
+    "South Korea": "kr",
+    "Spain": "es",
+    "Sri Lanka": "lk",
+    "Suriname": "sr",
+    "Swaziland": "sz",
+    "Sweden": "se",
+    "Switzerland": "ch",
+    "Syria": "sy",
+    "Taiwan": "tw",
+    "Tajikistan": "tj",
+    "Thailand": "th",
+    "Timor-leste": "tl",
+    "Togo": "tg",
+    "Tonga": "to",
+    "Trinidad And Tobago": "tt",
+    "Turkey": "tr",
+    "Tuvalu": "tv",
+    "Ukraine": "ua",
+    "Uganda": "ug",
+    "United Arab Emirates": "ae",
+    "United Kingdom": "gb",
+    "United States Of America": "us",
+    "Uruguay": "uy",
+    "Uzbekistan": "uz",
+    "Vanuatu": "vu",
+    "Zambia": "zm",
+}
+
 parser = ArgumentParser()
 parser.add_argument('-c')
 parser.add_argument('-i', default='econ')
-args = parser.parse_args()
+args = parser.parse_args() 
 
-# INSERT YOUR OWN KEY
-key = 'DEMO' 
+# USE YOUR OWN KEYS
+key = 'DEMO'
+news_key = 'DEMO'
+
 def main():
 
     # If no arguments are given when running, prompt for them.
@@ -85,12 +256,11 @@ def main():
     # Output depending on infoType:
 
     if infoType == 'econ':
-        conversion = econ(country)
-        gbp_conversion = 1/ float(conversion)
-        print(f"The current rate of {country_list[country]} to GBP is £{conversion} \n £1 = {gbp_conversion}")
+        econ(country)
+
 
     elif infoType == 'news':
-         news(country)
+         news(country.title())
 
     elif infoType == 'politics':
          ...
@@ -98,10 +268,9 @@ def main():
     else:
         print("Expected infoType argument, none given.")
 
-def econ(country):
+def econ(country, graph='y'):
         try:
-            coin = country_list[country]  
-        
+            coin = country_currency[country] 
         except KeyError:
              print("Country does not exist")
              pass
@@ -112,20 +281,60 @@ def econ(country):
         data = r.json()
 
         try:
-            return(float(data["Realtime Currency Exchange Rate"]["5. Exchange Rate"]))
-
+            conversion = (float(data["Realtime Currency Exchange Rate"]["5. Exchange Rate"]))
+            gbp_conversion = 1/ float(conversion)
+            print(f"The current rate of {country_currency[country]} to GBP is £{conversion} \n £1 = {gbp_conversion}")      
+            
         except KeyError:
             print("Coin does not exist")
             pass
 
-def news(country):
+        # SECOND SOURCE (OECD) For more detailed info
+        
+        # url = f'http://stats.oecd.org/SDMX-JSON/data/<dataset identifier>/<filter expression>/<agency name>[ ?<additional parameters>]'
+        
+        if not(graph):
+            graph = (input("Display graph? [Y/N]").lower() == 'y')
 
-        url = f'https://www.alphavantage.co/query?function=NEWS_SENTIMENT&topics={country}&tickers=AAPL&apikey={key}'
+        while graph:
+            dateRange = int(input("1. Week 2. Month 3. Year :"))
+
+            if dateRange == 2:
+
+                url = f'https://www.alphavantage.co/query?function=FX_MONTHLY&from_symbol=EUR&to_symbol=USD&apikey=demo'
+                r = requests.get(url)
+                data = r.json()
+
+                dates = [dates for dates in data['Time Series FX (Monthly)']]
+                print('DATES SUCCESS')
+                exchange_rates = [(data['Time Series FX (Monthly)'][dates]['4. close']) for dates in data['Time Series FX (Monthly)']]
+
+
+            plt.plot(dates, exchange_rates, marker='o')
+            plt.title(f'{country_currency[country]} to GBP Exchange Rates')
+            plt.xlabel('Date')
+            plt.ylabel('Exchange Rate')
+            plt.grid(True)
+            plt.show()
+        
+
+            graph = False
+
+def news(country):
+        
+        try:
+             country = country_codes[country]
+
+        except KeyError:
+             print("Your country does not exist, please refer to an official country names list")
+             pass
+        # url = f'https://www.alphavantage.co/query?function=NEWS_SENTIMENT&topics={country}&tickers=AAPL&apikey={key}'
+        url = f'https://api.worldnewsapi.com/search-news?source-countries={country}&language=en&api-key={news_key}'
         r = requests.get(url)
         data = r.json()
 
         try:
-            output = [article['title'] for article in data['feed']]
+            output = [article['title'] for article in data['news']]
             for _ in range(10):
                 print(f"{_}. {output[_]}\n")
             
